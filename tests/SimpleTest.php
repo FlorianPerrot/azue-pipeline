9695c58452c1b2dae4ada92e1b9a3f513d50b9f9
Florian Perrot <florian.perrot@wizbii.com>
2021-01-11 20:53:21 +0100

    Init

---
+++ tests/SimpleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testSimple()
+    {
+        $this->assertTrue(true);
+    }
+}
